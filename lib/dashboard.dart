e25df3eb9b52a327934f713f924a59f057f4cbb7
Akash Rajoria <akashrajoria250103@gmail.com>
2023-02-12 04:52:27 +0530

    earthquake algorithms

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -3,6 +3,8 @@ import 'package:geocoding/geocoding.dart';
 // import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:rapid_rescue/Models/earthquake.dart';
+import 'package:rapid_rescue/screens/loading.dart';
+import 'package:rapid_rescue/screens/safe_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key? key}) : super(key: key);
@@ -15,6 +17,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
   final Geolocator geolocator = Geolocator();
   Position? _currentPosition;
   EarthQuake? _earthQuake = new EarthQuake(0, 0, 0, new DateTime.now(), Color(0xFFe83030));
+  bool _loading = true;
+  bool _isSafe = true;
   @override
   void initState() {
     super.initState();
@@ -33,7 +37,10 @@ class _DashboardScreenState extends State<DashboardScreen> {
       _currentPosition = position;
       _earthQuake = earthQuake;
       if(_earthQuake==null)print("safe");
-      else print("unsafe");
+      else{
+        _isSafe = false;
+      }
+      _loading = false;
     });
     print(_currentPosition!.longitude);
 
@@ -44,52 +51,55 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("DASHBOARD"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).canvasColor,
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Icon(Icons.location_on),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'Location',
-                                style: Theme.of(context).textTheme.caption,
-                              ),
-                              if (_currentPosition != null )
-                                Text(_currentPosition!.latitude.toString() + "  " + _currentPosition!.longitude.toString() + " " ,
-                                    style:
-                                    Theme.of(context).textTheme.bodyText2),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          width: 8,
-                        ),
-                      ],
-                    ),
-                  ],
-                ))
-          ],
-        ),
-      ),
-    );
+    if(_loading)return LoadingScreen();
+    else if(_isSafe)return SafeScreen();
+    else return Scaffold();
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text("DASHBOARD"),
+    //   ),
+    //   body: SingleChildScrollView(
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: <Widget>[
+    //         Container(
+    //             decoration: BoxDecoration(
+    //               color: Theme.of(context).canvasColor,
+    //             ),
+    //             padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+    //             child: Column(
+    //               children: <Widget>[
+    //                 Row(
+    //                   children: <Widget>[
+    //                     Icon(Icons.location_on),
+    //                     SizedBox(
+    //                       width: 8,
+    //                     ),
+    //                     Expanded(
+    //                       child: Column(
+    //                         crossAxisAlignment: CrossAxisAlignment.start,
+    //                         children: <Widget>[
+    //                           Text(
+    //                             'Location',
+    //                             style: Theme.of(context).textTheme.caption,
+    //                           ),
+    //                           if (_currentPosition != null )
+    //                             Text(_currentPosition!.latitude.toString() + "  " + _currentPosition!.longitude.toString() + " " ,
+    //                                 style:
+    //                                 Theme.of(context).textTheme.bodyText2),
+    //                         ],
+    //                       ),
+    //                     ),
+    //                     SizedBox(
+    //                       width: 8,
+    //                     ),
+    //                   ],
+    //                 ),
+    //               ],
+    //             ))
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
